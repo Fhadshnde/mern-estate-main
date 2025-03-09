cf1182c13300729c8d903f7828de6ef71cc0ab7b
Fhadshnde <Fhadshnde32@gmail.com>
2025-03-10 00:06:44 +0300

    Fixed api tracking issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000/',
+        target: process.env.VITE_API_BASE_URL || 'http://localhost:3000',
+        changeOrigin: true,
         secure: false,
       },
     },
   },
-
   plugins: [react()],
 });
